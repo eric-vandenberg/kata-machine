c5d3d2757df90cc71b6e1f92c76c1947de99f231
Eric Vandenberg <contact@ericvandenberg.com>
2024-04-21 15:36:07 -0600

    feat: breadth first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,21 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  const queue: (BinaryNode<number | null>)[] = [head];
 
+  while (queue.length) {
+    const curr = queue.shift() as BinaryNode<number | null>;
+
+    if (curr.value === needle) {
+      return true;
+    }
+
+    if (curr.left) {
+      queue.push(curr.left);
+    }
+
+    if (curr.right) {
+      queue.push(curr.right);
+    }
+  }
+
+  return false;
 }
